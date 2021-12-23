516c41feb6320218d97d16d2f414acdfc9d61108
Bassam <bm083@hdm-stuttgart.de>
2021-12-23 12:06:55 +0100

    Images werden aus der API geholt und können nach links/rechts geswipt werden

--- client/src/pages/MatchingPage.tsx
+++ client/src/pages/MatchingPage.tsx
@@ -1,19 +1,77 @@
-import { FC } from "react";
-import Layout from "../components/LayoutComponent/Layout";
+import { FC, useState } from "react";
+import Layout from "../components/layoutComponent/Layout";
 import { Page, useNavigation } from "../utils/hooks/useNavigation";
+import "../styles/MatchingPage.styles.scss";
+import likeimage from '../assets/images/like.svg';
+import dislikeimage from '../assets/images/dislike.svg';
+import SwipableCard from "../components/swipableCard/SwipableCard";
 
 interface MatchingPageProps {}
 
+var executed = false;
+
 const MatchingPage: FC<MatchingPageProps> = ({}) => {
   const { currentLocation, onLocationChange } = useNavigation(Page.MATCHING);
+
+  const [images, setImages] = useState<any[]>([]);
+
+  const divStyle = {
+    marginTop: '30px',
+    height: '80%',
+    width: '100%',
+    color: 'blue',
+    borderRadius: '25px'
+  };
+
+
+  function likeImage() {
+    console.log("Klappt")
+  }
+
+  function dislikeImage() {
+    console.log("Klappt")
+  }
+
+  // Funktion spinnt noch etwas
+  // Die Idee ist, die Funktion lediglich einmal aufzurufen
+  var getImages = (function() {
+    return function() {
+        if (!executed) {
+          executed = true;
+          fetch('http://yumatch.mi.hdm-stuttgart.de/api/data/meal/some?count=10', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          })
+          .then((response) => response.json())
+          .then((data) => {
+            setImages([...images, data[1].strMealThumb])
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
+        }
+    };
+  })();
+
+  getImages();
+
   return (
     <Layout
       navigationElements={Object.entries(Page).map((page) => ({
         title: page[1],
       }))}
       changeLocation={onLocationChange}
-      currentLocation={currentLocation}
-    ></Layout>
+      currentLocation={currentLocation}>
+      <SwipableCard>
+        <img style={divStyle} src={images[images.length - 1]} />
+      </SwipableCard>
+      <div className="matching-buttons">
+        <span onClick={likeImage} className="dot"><img className="center" src={likeimage} /></span>
+        <span onClick={dislikeImage} className="dot"><img className="center" src={dislikeimage} /></span>
+      </div>
+    </Layout>
   );
 };
 

74974fbee2e97726945dcf166f96d3e24099b9ea
fadelkaadan <fadelkaadan@gmail.com>
2022-01-21 14:43:05 +0100

    Add (new post) button to profile

--- client/src/components/Posts/Posts.tsx
+++ client/src/components/Posts/Posts.tsx
@@ -1,5 +1,6 @@
 import { PostsProps } from "./Posts.types";
 import "./PostsStyles.scss";
+import { ReactComponent as AddIcon } from "../../assets/icons/add.svg";
 
 // TODO: fetch real data from backend
 const dummyData = [
@@ -27,8 +28,15 @@ const dummyData = [
 ];
 
 const Posts: React.FC<PostsProps> = ({ posts }) => {
+  const addPost = () => {};
+
   return (
     <div className="posts">
+      <button className="posts-add" onClick={addPost}>
+        <span>
+          <AddIcon />
+        </span>
+      </button>
       {dummyData.map((post, index) => (
         <img className="posts-item" src={post.image} alt="" key={index} />
       ))}
--- client/src/components/UserPosts/UserPosts.types.ts
+++
@@ -1,3 +0,0 @@
-export interface UserPostsProps {
-  posts: any[];
-}

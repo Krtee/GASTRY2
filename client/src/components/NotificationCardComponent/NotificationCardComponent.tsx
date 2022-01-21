154c4a35f91ed1ecfada01abf96d2b7818d97850
Domenico Ferrari <domenico@Domenicos-MacBook-Pro.local>
2022-01-21 18:52:27 +0100

    Current state

--- client/src/components/NotificationCardComponent/NotificationCardComponent.tsx
+++ client/src/components/NotificationCardComponent/NotificationCardComponent.tsx
@@ -1,30 +1,62 @@
-import { useHistory } from "react-router";
+import { ReactElement } from "react";
+import { ReactComponent as MatchIcon } from "../../assets/icons/Match.svg";
+import { ReactComponent as FriendIcon } from "../../assets/icons/friend-plus.svg";
+import { NotificationType } from "../../utils/notification/Notification.types";
+import "./NotificationCardComponent.styles.scss";
 
-interface NotificationCardComponentProps {
-  className: string;
+export interface NotificationCardComponentProps {
+  className?: string;
   title: string;
   message: string;
-  matchId?: string;
+  onClick?(): void;
+  icon?: ReactElement;
+  seen?: boolean;
+  notificationType: NotificationType;
+  index?: number;
 }
 
 const NotificationCardComponent: React.FC<NotificationCardComponentProps> = ({
   className = "",
   title,
   message,
-  matchId,
+  onClick,
+  seen,
+  notificationType,
+  index = 0,
   children,
 }) => {
-  const history = useHistory();
+  const getNotificationIcon = (): ReactElement => {
+    switch (notificationType) {
+      case NotificationType.MULTI_MATCH:
+        return <MatchIcon />;
+      case NotificationType.BUDDY_REQUEST:
+        return (
+          <div className="friend">
+            <FriendIcon />
+          </div>
+        );
+      default:
+        return <FriendIcon />;
+    }
+  };
 
   return (
     <div
       className={["notification-card--wrapper ", className].join("")}
-      onClick={() =>
-        matchId ? history.push(`/matching/result/${matchId}`) : () => {}
-      }
+      onClick={onClick}
+      style={{ animationDelay: `${0.1 * index}s` }}
     >
-      <p className="notification-card--title"> {title}</p>
-      <p className="notification-card--message"> {message}</p>
+      <div className="notification-card--icon-container">
+        {getNotificationIcon()}
+      </div>
+      <div className="notification-card--text">
+        <p className="notification-card--title"> {title}</p>
+        <p className="notification-card--message"> {message}</p>
+      </div>
+
+      <div
+        className={["notification-card--dot ", seen ? "" : "new"].join("")}
+      />
     </div>
   );
 };
--- client/src/pages/NotificationPage.tsx
+++ client/src/pages/NotificationPage.tsx
@@ -2,28 +2,72 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from "recoil";
 import Layout from "../components/LayoutComponent/Layout";
+import NotificationCardComponent, {
+  NotificationCardComponentProps,
+} from "../components/NotificationCardComponent/NotificationCardComponent";
 import { Page, useNavigation } from "../utils/hooks/useNavigation";
-import { NotificationPageProps } from "../utils/notification/Notification.types";
+import { ReactComponent as ArrowLeftIcon } from "../assets/icons/arrow_left.svg";
+import {
+  NotificationPageProps,
+  NotificationType,
+} from "../utils/notification/Notification.types";
 import { userState } from "../utils/user/User.state";
+import "../styles/NotificationPage.styles.scss";
+import { useHistory } from "react-router";
 
 const NotificationPage: React.FC<NotificationPageProps> = () => {
   const { t } = useTranslation();
+  const history = useHistory();
   const { currentLocation, onLocationChange, navItems } = useNavigation(
     Page.NOTIFICATION
   );
   const user = useRecoilValue(userState);
+  const demoNoti: NotificationCardComponentProps[] = [
+    {
+      title: "TestNoti",
+      message: "This is a test noti",
+      notificationType: NotificationType.BUDDY_REQUEST,
+    },
+    {
+      title: "TestNoti",
+      message: "This is a test noti",
+      notificationType: NotificationType.BUDDY_REQUEST,
+    },
+    {
+      title: "TestNoti",
+      message: "This is a test noti",
+      notificationType: NotificationType.BUDDY_REQUEST,
+    },
+    {
+      title: "TestNoti",
+      message: "This is a test noti",
+      notificationType: NotificationType.MULTI_MATCH,
+    },
+    {
+      title: "TestNoti",
+      message: "This is a test noti",
+      notificationType: NotificationType.MULTI_MATCH,
+      seen: true,
+    },
+  ];
   return (
     <Layout
       navigationElements={navItems}
       changeLocation={onLocationChange}
       currentLocation={currentLocation}
-      hideBar
+      header={{
+        leftIconButton: {
+          value: <ArrowLeftIcon />,
+          onClick: () => history.goBack(),
+        },
+        title: t("general.navigation.NOTIFICATION"),
+      }}
     >
       <div className="notification-page--wrapper">
-
-      
+        {demoNoti.map((noti, index) => (
+          <NotificationCardComponent {...noti} index={index} />
+        ))}
       </div>
-
     </Layout>
   );
 };
